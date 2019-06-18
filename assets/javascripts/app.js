adbc5a4398164da007bd12f20b466a07bf1e0548
川崎暑士 <kawasaki8910@gmail.com>
2019-06-18 14:47:17 +0900

    hit check

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -3,19 +3,24 @@ import _ from "lodash"
 const BLOCK_SIZE = 24
 const BLOCK_RAWS = 22
 const BLOCK_COLS = 12
-const NORMAL_BLOCK = 1
 
+const NORMAL_BLOCK = 1
 const NON_BLOCK = 0
 const WALL = 9
+const LOCK_BLOCK = 2
+
 const BACK_COLOR = "#ddd"
 const WALL_COLOR = "#666"
 const BLOCK_COLOR = "#000"
+const LOCK_COLOR = "#333"
 
 const SCREEN_WIDTH = BLOCK_SIZE * BLOCK_COLS
 const SCREEN_HEIGHT = BLOCK_SIZE * BLOCK_RAWS
 
 let blockX = 0
 let blockY = 0
+let oldBlockX = 0
+let oldBlockY = 0
 
 let currentBlock = []
 
@@ -97,6 +102,9 @@ const CONTEXT = canvas.getContext("2d");
 let field = _.cloneDeep(STAGE)
 
 function draw() {
+  CONTEXT.fillStyle = BACK_COLOR
+  CONTEXT.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
+
   for(let row = 0; row < BLOCK_RAWS; row++) {
     for(let col = 0; col < BLOCK_COLS; col++) {
       switch(field[row][col]){
@@ -105,6 +113,11 @@ function draw() {
           break
         case NORMAL_BLOCK:
           CONTEXT.fillStyle = BLOCK_COLOR
+          break
+        case LOCK_BLOCK:
+          CONTEXT.fillStyle = LOCK_COLOR
+          console.log('lock')
+          break;
         case WALL:
           CONTEXT.fillStyle = WALL_COLOR
           break
@@ -118,8 +131,8 @@ function createBlock() {
   const blockType = Math.floor(Math.random() * BLOCKS.length)
 
   // stageの中心
-  blockX = Math.floor(BLOCK_COLS / 3)
-  blockY = 0
+  blockX = oldBlockX = Math.floor(BLOCK_COLS / 3)
+  blockY = oldBlockY = 0
 
   currentBlock = _.cloneDeep(BLOCKS[blockType])
 }
@@ -147,18 +160,48 @@ function clearBlock () {
   }
 }
 
-let speed = 1500
+function lockBlock () {
+  for (let row = 0; row < currentBlock.length; row++) {
+    for (let col = 0; col  < currentBlock[row].length; col++) {
+      if (currentBlock[row][col]) {
+        // console.log(currentBlock[row][col])
+        if (currentBlock[row][col]) {
+          console.log(LOCK_BLOCK)
+          field[row + blockY][col  + blockX] = LOCK_BLOCK
+        }
+      }
+    }
+  }
+}
+
+function hitCheck () {
+  for (let row = 0; row < currentBlock.length; row++) {
+    for (let col = 0; col  < currentBlock[row].length; col++) {
+      if (field[row + blockY][col  + blockX] && currentBlock[row][col]) return true
+    }
+  }
+  return false
+}
+
+let speed = 500
 let lastUpdate = 0
 
 function loop (timestamp) {
+  oldBlockX = blockX
+  oldBlockY = blockY
+
   const diff = timestamp - lastUpdate
   if (diff > speed) {
     lastUpdate = timestamp
     clearBlock()
     blockY++
+    if (hitCheck()) {
+      blockY = oldBlockY
+      lockBlock()
+      createBlock()
+    }
     updateBlock()
-    CONTEXT.fillStyle = BACK_COLOR
-    CONTEXT.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
+
     draw()
   }
 
