6ff8ddd2c52d0dd8b5442efd3a4c3b8f634e4335
川崎暑士 <kawasaki8910@gmail.com>
2019-06-19 18:23:14 +0900

    typo

--- assets/javascripts/tetris.js
+++ assets/javascripts/tetris.js
@@ -1,11 +1,11 @@
 import _ from "lodash"
 
 const BLOCK_SIZE = 24
-const BLOCK_RAWS = 22
+const BLOCK_ROWS = 22
 const BLOCK_COLS = 12
 
 const SCREEN_WIDTH = BLOCK_SIZE * BLOCK_COLS
-const SCREEN_HEIGHT = BLOCK_SIZE * BLOCK_RAWS
+const SCREEN_HEIGHT = BLOCK_SIZE * BLOCK_ROWS
 
 export default class Tetris {
   constructor (canvas) {
