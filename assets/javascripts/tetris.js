d9875578209847e38908fb8f76468f3e0d8009f5
川崎暑士 <kawasaki8910@gmail.com>
2019-06-19 19:20:09 +0900

    set wall

--- assets/javascripts/tetris.js
+++ assets/javascripts/tetris.js
@@ -1,3 +1,6 @@
+/*
+ * Canvasを利用してステージを描画
+ */
 import _ from "lodash"
 
 const BLOCK_SIZE = 24
@@ -7,10 +10,62 @@ const BLOCK_COLS = 12
 const SCREEN_WIDTH = BLOCK_SIZE * BLOCK_COLS
 const SCREEN_HEIGHT = BLOCK_SIZE * BLOCK_ROWS
 
+const NON_BLOCK = 0
+const WALL = 9
+
+const BACK_COLOR = "#f5f5f5"
+const WALL_COLOR = "#000000"
+
+const STAGE = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+  [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+  [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+  [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+  [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+  [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+  [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+  [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+  [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+  [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+  [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+  [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+  [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+  [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+  [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+  [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+  [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+  [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+  [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+  [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+
 export default class Tetris {
   constructor (canvas) {
     canvas.width = SCREEN_WIDTH
     canvas.height = SCREEN_HEIGHT
     this.cxt = canvas.getContext("2d")
+
+    this.stage = _.cloneDeep(STAGE)
+    this.draw()
+  }
+
+  draw () {
+    for (let row = 0; row < BLOCK_ROWS; row++) {
+      for (let col = 0; col < BLOCK_COLS; col++) {
+        switch(this.stage[row][col]){
+          case NON_BLOCK:
+            this.cxt.fillStyle = BACK_COLOR
+            break
+          case WALL:
+            this.cxt.fillStyle = WALL_COLOR
+            break
+        }
+        this.cxt.fillRect(col * BLOCK_SIZE, row * BLOCK_SIZE, BLOCK_SIZE -1, BLOCK_SIZE - 1)
+      }
+    }
   }
 }
